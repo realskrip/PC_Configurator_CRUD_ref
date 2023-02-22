09896bfa23e38f376ddbb1ea3fa97ea08efca6a7
Skripnikov Danila <19eazy@gmail.com>
2023-02-22 16:46:51 +0700

    0.1.1 Добавлены модели SystemUnit и ApplicationContext.

---
+++ PC configurator/Models/ApplicationContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PC_configurator.Models
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<SystemUnit> SystemUnits { get; set; } = null!;
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+        {
+            Database.EnsureCreated();   // создаем базу данных при первом обращении
+        }
+    }
+}
---
+++ PC configurator/Models/SystemUnit.cs
@@ -0,0 +1,17 @@
+﻿namespace PC_configurator.Models
+{
+    public class SystemUnit
+    {
+        public int Id { get; set; }
+        public string? CPU { get; set; }
+        public string? ComputerCase { get; set; }
+        public string? Motherboard { get; set; }
+        public string? GPU { get; set; }
+        public string? CPUcooler { get; set; }
+        public string? RAM { get; set; }
+        public string? HDD { get; set; }
+        public string? SSD { get; set; }
+        public string? PowerSupply { get; set; }
+        public string? SoundСard { get; set; }
+    }
+}
