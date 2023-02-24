0332a44ee7dc9b42ae07aa4b8ba5b155f13a0da1
Skripnikov Danila <19eazy@gmail.com>
2023-02-24 21:06:38 +0700

    0.2.0 Подключена база данных, добавлена возможность выполнять добавлять и выводить данные.

---
+++ PC configurator/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PC_configurator.Models;
+
+namespace PC_configurator.Controllers
+{
+    public class HomeController : Controller
+    {
+        ApplicationContext db;
+        public HomeController(ApplicationContext context)
+        {
+            db = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await db.SystemUnits.ToListAsync());
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(SystemUnit systemUnit)
+        {
+            db.SystemUnits.Add(systemUnit);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+    }
+}
--- PC configurator/Program.cs
+++ PC configurator/Program.cs
@@ -1,6 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using PC_configurator.Models;    // пространство имен класса ApplicationContext
+
 var builder = WebApplication.CreateBuilder(args);
+
+// получаем строку подключения из файла конфигурации
+string? connection = builder.Configuration.GetConnectionString("DefaultConnection");
+
+// добавляем контекст ApplicationContext в качестве сервиса в приложение
+builder.Services.AddDbContext<ApplicationContext>(options => options.UseSqlServer(connection));
+
+builder.Services.AddControllersWithViews();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapDefaultControllerRoute();
 
 app.Run();
